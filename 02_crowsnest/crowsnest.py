6bb3b44a41a5108d25f8279d34b56c0349a3b901
Carlos Meza <carlos@digitalr00ts.com>
2022-03-02 01:10:11 -0800

    feat(02_crowsnet): merge get_article into get_message

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -3,7 +3,7 @@
 
 import argparse
 import sys
-from typing import Any, Literal
+from typing import Any
 
 SIDES = ("larboard", "starboard")
 
@@ -30,25 +30,6 @@ def get_args(args: list[Any] | None = None) -> argparse.Namespace:
     return parser.parse_args(args)
 
 
-def get_article(word: str) -> Literal["a", "an", "A", "An"]:
-    """Return article 'an' if word begins with a vowel, otherwise return 'a'.
-
-    >>> get_article("consonants")
-    'a'
-    >>> get_article("apple")
-    'an'
-    >>> get_article("Consonants")
-    'A'
-    >>> get_article("Apple")
-    'An'
-    >>> word="example"
-    >>> f"This is {get_article(word)} {word}."
-    'This is an example.'
-    """
-    article = "an" if word.casefold().startswith(("a", "e", "i", "o", "u")) else "a"
-    return article if word.islower() else article.title()
-
-
 def is_word_ok(word: str) -> bool:
     """Raise exception if word is invalid.
 
@@ -72,7 +53,9 @@ def get_message(word: str, side: str) -> str:
     >>> get_message("Shamu", "starboard")
     'Ahoy, Captain, A Shamu off the starboard bow!'
     """
-    return f"Ahoy, Captain, {get_article(word)} {word} off the {side} bow!"
+    article = "an" if word.casefold().startswith(("a", "e", "i", "o", "u")) else "a"
+    article = article if word.islower() else article.title()
+    return f"Ahoy, Captain, {article} {word} off the {side} bow!"
 
 
 def main():
