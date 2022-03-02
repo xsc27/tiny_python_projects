88ae49b4f0f915ce0850e5cd466460ff163d6adf
Carlos Meza <carlos@digitalr00ts.com>
2022-03-02 01:04:04 -0800

    feat(02_crowsnet): add "going further"

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -2,7 +2,10 @@
 """crowsnet module."""
 
 import argparse
+import sys
 from typing import Any, Literal
+
+SIDES = ("larboard", "starboard")
 
 
 def get_args(args: list[Any] | None = None) -> argparse.Namespace:
@@ -10,33 +13,73 @@ def get_args(args: list[Any] | None = None) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Crow's Nest -- choose the correct article",
         allow_abbrev=False,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
         "str",
         help="A word",
     )
+    parser.add_argument(
+        "-s",
+        "--side",
+        choices=SIDES,
+        default="larboard",
+        help="Select side.",
+    )
 
     return parser.parse_args(args)
 
 
-def get_article(word: str) -> Literal["a", "an"]:
+def get_article(word: str) -> Literal["a", "an", "A", "An"]:
     """Return article 'an' if word begins with a vowel, otherwise return 'a'.
 
     >>> get_article("consonants")
     'a'
     >>> get_article("apple")
     'an'
+    >>> get_article("Consonants")
+    'A'
+    >>> get_article("Apple")
+    'An'
     >>> word="example"
     >>> f"This is {get_article(word)} {word}."
     'This is an example.'
     """
-    return "an" if word.casefold().startswith(("a", "e", "i", "o", "u")) else "a"
+    article = "an" if word.casefold().startswith(("a", "e", "i", "o", "u")) else "a"
+    return article if word.islower() else article.title()
+
+
+def is_word_ok(word: str) -> bool:
+    """Raise exception if word is invalid.
+
+    >>> is_word_ok("Yes")
+    True
+    >>> is_word_ok("!nope")
+    Traceback (most recent call last):
+    ValueError: '!nope' does not start with an alphabetic character.
+    """
+    if not word[0].isalpha():
+        sys.tracebacklimit = 0
+        raise ValueError(f"'{word}' does not start with an alphabetic character.")
+    return True
+
+
+def get_message(word: str, side: str) -> str:
+    """Get message.
+
+    >>> get_message("unicorn", "larboard")
+    'Ahoy, Captain, an unicorn off the larboard bow!'
+    >>> get_message("Shamu", "starboard")
+    'Ahoy, Captain, A Shamu off the starboard bow!'
+    """
+    return f"Ahoy, Captain, {get_article(word)} {word} off the {side} bow!"
 
 
 def main():
     """Run main logic."""
     args = get_args()
-    print(f"Ahoy, Captain, {get_article(args.str)} {args.str} off the larboard bow!")
+    is_word_ok(args.str)
+    print(get_message(args.str, args.side))
 
 
 if __name__ == "__main__":
